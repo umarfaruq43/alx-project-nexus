ed1c775bb89c0327bad3335c9c10442c6085492e
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2025-12-03 17:41:47 +0100

    feat: add PWA support with service worker registration and manifest file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,18 @@ import Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        if ("serviceWorker" in navigator) {
+            window.addEventListener("load", () => {
+                navigator.serviceWorker.register("/sw.js");
+            });
+        }
+    }, []);
+
     return (
         <main className={inter.className}>
             <Layout>
