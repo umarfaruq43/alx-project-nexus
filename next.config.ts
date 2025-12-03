4d91998263b5de1ac048291383a40cf041845b00
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2025-12-03 17:40:57 +0100

    chore: update dependencies and add new packages
    - Added @ducanh2912/next-pwa, @supabase/supabase-js, axios, and next-pwa to dependencies
    - Updated webpack to version 5.103.0

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,29 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-    /* config options here */
+// const nextConfig: NextConfig = {
+//     /* config options here */
+//     reactStrictMode: true,
+//     images: {
+//         domains: ["images.unsplash.com", "image.tmdb.org"],
+//     },
+// };
+
+// export default nextConfig;
+
+const withPWA = require("next-pwa")({
+    dest: "public",
+    // disable: process.env.NODE_ENV === "development",
+    register: true,
+    skipWaiting: true,
+    buildExcludes: [/middleware-manifest.json$/],
+});
+
+const nextConfig = {
     reactStrictMode: true,
     images: {
         domains: ["images.unsplash.com", "image.tmdb.org"],
     },
+    turbopack: {},
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
